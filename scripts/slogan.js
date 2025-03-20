042c0a8c72346d68ccf5daf4d968d53098481aa6
worranhin <worranhin@foxmail.com>
2025-03-20 22:53:05 +0800

    修复 slogan 不能出现英文逗号的bug；同时添加了更多slogan

--- scripts/slogan.js
+++ scripts/slogan.js
@@ -1,4 +1,3 @@
-﻿
-hexo.extend.filter.register('theme_inject', function (injects) {
+﻿hexo.extend.filter.register('theme_inject', function (injects) {
   injects.bodyEnd.file('slogan', 'source/_inject/slogan.ejs', {}, {}, -1);
 });
